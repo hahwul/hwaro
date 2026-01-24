9824d11dcac4f4cceca52ee3ad50ac76482aec00
hahwul <hahwul@gmail.com>
2026-01-24 17:47:30 +0900

    u

--- spec/hwaro_spec.cr
+++ spec/hwaro_spec.cr
@@ -4,7 +4,7 @@ describe Hwaro do
   describe "VERSION" do
     it "has a version number" do
       Hwaro::VERSION.should_not be_nil
-      Hwaro::VERSION.should eq("0.0.4")
+      Hwaro::VERSION.should eq("0.0.5")
     end
   end
 
