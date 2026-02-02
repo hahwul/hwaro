694e01e32322d1a271d5cd19f9198c5b3287763a
HAHWUL <hahwul@gmail.com>
2026-02-02 21:30:53 +0900

    Update version
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- spec/hwaro_spec.cr
+++ spec/hwaro_spec.cr
@@ -4,7 +4,7 @@ describe Hwaro do
   describe "VERSION" do
     it "has a version number" do
       Hwaro::VERSION.should_not be_nil
-      Hwaro::VERSION.should eq("0.0.8")
+      Hwaro::VERSION.should eq("0.0.9")
     end
   end
 
--- src/hwaro.cr
+++ src/hwaro.cr
@@ -89,5 +89,5 @@ require "./content/hooks"
 require "./cli/runner"
 
 module Hwaro
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
 end
