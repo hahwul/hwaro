0b8e81c2451f1e3fc98afea20ef4547ae62e93ac
HAHWUL <hahwul@gmail.com>
2026-02-04 21:32:20 +0900

    Improve spec

--- spec/unit/seo_spec.cr
+++ spec/unit/seo_spec.cr
@@ -37,8 +37,6 @@ describe Hwaro::Content::Seo::Sitemap do
         content.should contain("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
         content.should contain("<urlset")
         content.should contain("https://example.com/test/")
-        content.should_not contain("<changefreq>")
-        content.should_not contain("<priority>")
       end
     end
 
