1e228876101271f72b6bd6ce31a9245fed296b67
HAHWUL <hahwul@gmail.com>
2026-05-05 00:48:06 +0900

    Release v0.13.1 (#519)

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
         hwaro = pkgs.crystal.buildCrystalPackage rec {
           pname = "hwaro";
-          version = "0.13.0";
+          version = "0.13.1";
 
           src = ./.;
 
--- src/hwaro.cr
+++ src/hwaro.cr
@@ -98,5 +98,5 @@ require "./content/hooks"
 require "./cli/runner"
 
 module Hwaro
-  VERSION = "0.13.0"
+  VERSION = "0.13.1"
 end
